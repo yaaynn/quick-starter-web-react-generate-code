002e182a5197fcbe77e1298a27df0430e4ddd534
yaaynn <yaaynn@gmail.com>
2025-04-15 15:25:20 +0800

    添加复制到粘贴板

--- src/pages/IndexPage/Components/GenerateCodePanel/components/GenerateCodeComponent/index.tsx
+++ src/pages/IndexPage/Components/GenerateCodePanel/components/GenerateCodeComponent/index.tsx
@@ -2,6 +2,7 @@ import { GenerateCodeResult } from "../../../../../../utils/nunjucks-util.tsx";
 import { Button, Input, Space } from "antd";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { useCore } from "../../../../../../providers/CoreProvider.tsx";
 
 export interface GenerateCodeComponentProps extends GenerateCodeResult {
   onOutputPathChange?: (outputPath: string) => void;
@@ -9,6 +10,8 @@ export interface GenerateCodeComponentProps extends GenerateCodeResult {
   loading?: boolean;
 }
 export const GenerateCodeComponent = (props: GenerateCodeComponentProps) => {
+  const { core } = useCore();
+
   function handleWrite2File() {
     props.onWrite2File?.(props.outputPath, props.content);
   }
@@ -17,13 +20,24 @@ export const GenerateCodeComponent = (props: GenerateCodeComponentProps) => {
       <Space direction={"vertical"} className={"w-full"}>
         <div className={"flex justify-between"}>
           <Input
-            style={{ width: "calc(100% - 112px)" }}
+            style={{ width: "calc(100% - 238px)" }}
             value={props.outputPath}
             onChange={(e) => props.onOutputPathChange?.(e.target.value)}
           />
-          <Button onClick={handleWrite2File} loading={props.loading}>
-            写入到文件
-          </Button>
+          <Space className={"ml-3"}>
+            <Button onClick={handleWrite2File} loading={props.loading}>
+              写入到文件
+            </Button>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(props.content).then();
+                core.showSuccessMessage("复制到粘贴板成功.");
+              }}
+              loading={props.loading}
+            >
+              复制到剪切板
+            </Button>
+          </Space>
         </div>
         <div style={{ maxHeight: "80vh" }} className={"overflow-auto"}>
           <div>
